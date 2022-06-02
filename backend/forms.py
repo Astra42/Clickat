94bf872d06dc56f1f400f3afab7c31dd16662da5
Astra <enshov@mail.ru>
2022-06-02 23:22:05 +0500

    added register, login, logout

---
+++ backend/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'password_confirm']
+
+    username = forms.CharField\
+    (
+        max_length=20,
+        label='Имя котика',
+        widget=forms.TextInput(attrs={'placeholder': 'жмякай лапками имя'}),
+    )
+    password = forms.CharField\
+    (
+        min_length=3,
+        max_length=20,
+        label='Пароль котика',
+        widget=forms.PasswordInput(attrs={'placeholder': 'тыкай парольчик'}),
+    )
+    password_confirm = forms.CharField\
+    (
+        min_length=3,
+        max_length=20,
+        label='Патверждение паролика',
+        widget=forms.PasswordInput(attrs={'placeholder': 'Вводи пароль ещё'}),
+    )
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        password = cleaned_data.get("password")
+        password_confirm = cleaned_data.get("password_confirm")
+        if password != password_confirm:
+            raise forms.ValidationError("Пароли разные!")
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data.get("password"))
+        if commit:
+            user.save()
+        return user
