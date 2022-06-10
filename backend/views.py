d966c56f8e2783149fd88cd802a6fcb2d652fed4
Алёна Полякова <alenushka524@gmail.com>
2022-06-10 23:36:34 +0500

    Registration on DRF

--- backend/views.py
+++ backend/views.py
@@ -3,35 +3,35 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .forms import UserForm
 from . import forms
+from rest_framework.views import APIView
+
 
 @login_required
 def index(request):
     return render(request, 'index.html', {})
 
-def register(request):
-    if(request.method == 'POST'):
+class Register(APIView):
+    def get(self, request):
+        form = UserForm()
+        return render(request, 'register.html', {'form': form})
+    def post(self, request):
         form = UserForm(request.POST)
-        if(form.is_valid):
+        if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('index')
-        return render(request, 'register.html', {"form": form})
 
-    form = UserForm()
-    return render(request, 'register.html', {"form":form})
+        return render(request, 'register.html', {'form': form})
 
 def user_login(request):
     form = UserForm()
-    if(request.method == 'POST'):
-        userName = request.POST.get("username")
-        password  = request.POST.get('password')
-        user = authenticate(username=userName, password=password)
-        if(user):
+    if request.method == 'POST':
+        user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
+        if user:
             login(request, user)
-            return redirect("index")
-        return render(request, 'login.html', {"form": form, 'invalid':True})
-    return render(request, 'login.html', {"form": form})
-
+            return redirect('index')
+        return render(request, 'login.html', {'form': form, 'invalid': True})
+    return render(request, 'login.html', {'form': form})
 
 
 @login_required
--- clickat/urls.py
+++ clickat/urls.py
@@ -20,4 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('backend.urls')),
+    path('api/', include('api.urls')),
+
+
 ]
